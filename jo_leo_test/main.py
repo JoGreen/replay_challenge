e5e0fdbcb694979602b12c41ea23ebf66b9c391b
JoGreen <giorgiomaria@live.it>
2018-03-14 00:21:24 +0100

    main

---
+++ jo_leo_test/main.py
@@ -0,0 +1,24 @@
+from jo_leo_test import Triangle
+from jo_leo_test import escaping_points
+from jo_leo_test.input_parser  import parse_file
+
+def useful_points(p):
+    if triangle.contains(*p):
+        points.remove(p)
+
+
+src, trg, triangles = parse_file('data.txt')
+triangles = []
+points = set()
+for triangle in triangles:
+    points = set.union(escaping_points(*triangle.get_vertices() ) )
+
+for triangle in triangles:
+    map(useful_points, points)
+
+
+
+
+
+
+
