992ae7f241bd14168edf72e1970650edd1d2f1d1
JoGreen <giorgiomaria@live.it>
2018-03-13 23:51:18 +0100

    escaping points

--- jo_leo_test/escaping_points.py
+++ jo_leo_test/escaping_points.py
@@ -8,12 +8,11 @@ def find_points(v1, v2, v3):
         coordinates_x = [x-1, x, x+1]
         coordinates_y = [y-1, y, y+1]
 
-        for x in coordinates_x:
-            for y in coordinates_y:
-                points.append((x,y) )
+        for px in coordinates_x:
+            for py in coordinates_y:
+                if (px, py) != v:
+                    points.append((px,py) )
 
-    print(len(points))
 
     points = set(points)
     return points
-
