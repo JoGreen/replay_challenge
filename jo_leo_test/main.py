525ada03f34aeb024badd21286b88d4a7006a914
JoGreen <giorgiomaria@live.it>
2018-03-14 00:35:19 +0100

    code refactor

--- jo_leo_test/main.py
+++ jo_leo_test/main.py
@@ -2,19 +2,20 @@ from jo_leo_test.triangle import triangle
 from jo_leo_test import escaping_points
 from jo_leo_test.input_parser  import parse_file
 
-def useful_points(p):
+def remove_useless_points(p):
     if triangle.contains(*p):
         points.remove(p)
+        print ('point removed')
 
 
 src, trg, triangles = parse_file('data.txt')
-triangles = []
+
 points = set()
 for triangle in triangles:
     points = set.union(escaping_points(*triangle.get_vertices() ) )
 
 for triangle in triangles:
-    map(useful_points, points)
+    map(remove_useless_points, points)
 
 
 
