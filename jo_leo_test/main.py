121258761c5b3347f8af872d487ba74efa884c7b
ClaudioCimarelli <cima.claudio@gmail.com>
2018-03-15 10:46:23 +0100

    itertools filterfalse

--- jo_leo_test/main.py
+++ jo_leo_test/main.py
@@ -9,10 +9,10 @@ from networkx.algorithms import has_path
 
 
 def filter_points_in_triangle(point_list):
-    return lambda tr: filter(lambda x: ~tr.contains(x), point_list)
+    return lambda tr: itertools.filterfalse(lambda x: tr.contains(x), point_list)
 
 
-src, dest, triangles = parse_file('test_input1.txt')
+src, dest, triangles = parse_file('input_1.txt')
 t0 = time.time()
 points = []
 
@@ -26,25 +26,27 @@ print(len(points))
 print(time.time()-t0)
 t0 = time.time()
 
-filter_all = map(filter_points_in_triangle(points), triangles)
-
-points = list(itertools.chain.from_iterable(filter_all))
-
-print(len(points))
-
-print(time.time()-t0)
-t0 = time.time()
-
 s = set(points)
 
-print(len(s))
-for i in s:
-    print(i)
 s.add(src)
 s.add(dest)
 
 print(time.time()-t0)
 t0 = time.time()
+
+filter_all = map(filter_points_in_triangle(points), triangles)
+
+print(time.time()-t0)
+t0 = time.time()
+
+#points = list(itertools.chain.from_iterable(filter_all))
+
+#print(len(points))
+
+print(time.time()-t0)
+t0 = time.time()
+
+
 
 G = graph.create(points)
 
